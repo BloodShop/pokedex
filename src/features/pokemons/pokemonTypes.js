af27d34c0b78100ff3400b8e04f97fbab4e43324
BloodShop <koliakovcr7@gmail.com>
2023-01-30 15:19:57 +0200

    !

--- src/features/pokemons/pokemonTypes.js
+++ src/features/pokemons/pokemonTypes.js
@@ -1,15 +1,16 @@
-const pokemonTypes = ['grass','fire','flying','water','bug','normal','electric','ground','fairy','fighting','psychic','rock','steel','ice','ghost','dragon'];
-export default pokemonTypes;
-
 // Creating the pokemonTypes - from getting all of the pokemons, and then set will avoid type duplicates and arrange it again to an array
 
-/* const dispatch = useDispatch()
-const pokemonTypes = () => {
+import pokedexService from "./pokemonService";
+
+(async () => {
     debugger
-    const pokemons = dispatch(getPokemons());
+    const pokemons = await pokedexService.getPokemons();
     const types = pokemons.reduce((acc, poke) => {
         acc.concat(poke.types);
         return acc;
     }, [])
     return [...new Set(types)]
-} */
+})();
+
+const pokemonTypes = ['grass','fire','flying','water','bug','normal','electric','ground','fairy','fighting','psychic','rock','steel','ice','ghost','dragon'];
+export default pokemonTypes;
